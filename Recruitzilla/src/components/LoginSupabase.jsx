eba9636dbb0560bab883bf093e5a5aa61b3030e5
Juska Kellokumpu <juskakaro.kellokumpu@tuni.fi>
2024-05-21 11:26:48 +0300

    RLS select and insert based on user.id

--- Recruitzilla/src/components/LoginSupabase.jsx
+++ Recruitzilla/src/components/LoginSupabase.jsx
@@ -85,6 +85,59 @@ const SignOutButton = () => {
   )
 }
 
+// testing inserting data and RLS
+const TestDataView = ({ session }) => {
+  const [testData, setTestData] = useState([])
+  //console.log("session", session)
+
+  useEffect(() => {
+    getTestData();
+  }, []);
+
+  async function getTestData() {
+    const { data } = await supabase.from("test_table").select();
+    setTestData(data);
+    console.log("data getTestData", data)
+  }
+
+  // in Supabase SQL editor, RLS rule to see only own row
+/*   alter table "data" enable row level security;
+
+  create policy "Individuals can view their own data."
+  on data for select
+  using ( (select auth.uid()) = user_id ); */
+
+  // RLS authenticated users can insert into data
+  const insertData = async () => {
+    const { error } = await supabase
+      .from("test_table")
+      .insert({
+        //id: Math.random(),
+        user_id: session.user.id,
+        data: "test data",
+        full_name: session.user.identities[0].identity_data.full_name,
+        email: session.user.identities[0].identity_data.email
+      })
+    getTestData()
+  }
+
+  return (
+    <div>
+      <ul>
+        {testData.map((data) => (
+          <li key={data.id}>{data.id} {data.user_id} {data.data} {data.full_name} {data.email}</li>
+        ))}
+      </ul>
+      <button
+        className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black"
+        onClick={insertData}
+        >
+          insert data
+        </button>
+    </div>
+  )
+}
+
 const LoginSupabase = () => {
   const [session, setSession] = useState(null)
   const navigate = useNavigate()
