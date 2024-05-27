23e94154b79caf9ce9dd0fb0fed2e8e9eddc4965
Juska Kellokumpu <juskakaro.kellokumpu@tuni.fi>
2024-05-27 13:01:19 +0300

    More cleanup

--- Recruitzilla/src/components/LoginSupabase.jsx
+++ Recruitzilla/src/components/LoginSupabase.jsx
@@ -59,93 +59,6 @@ const SignOutButton = () => {
   )
 }
 
-const initialUser = [
-  { id: 0, name: 'initial name', roles: { name: 'initial role' } }
-]
-
-const RoleSelectionButton = ({ session }) => {
-  const [users, setUsers] = useState([]);
-  const userId = session.user.id;
-
-  useEffect(() => {
-    getUsers();
-  }, []);
-
-  //Supabase doesn't have name by default using this and testuser for testing purposes 
-  async function getUsers() {
-    const { data, error } = await supabase
-      .from("users")
-      .select(`
-        id,
-        name,
-        roles (name)
-      `);
-    if (error) console.error(error);
-    setUsers(data);
-  }
-
-  const selectRole = async (role, userId) => {
-    const { data, error } = await supabase
-      .from("new_users")
-      .update({ role_name: role }) 
-      .eq("id", userId);
-    if (error) console.error(error);
-
-    getUsers();
-    set_claim(userId, 'userrole', role);
-  }
-
-  const set_claim = async (uid, claim, value) => {
-    try {
-      console.log('uid:', uid);
-      console.log('claim:', claim);
-      console.log('value:', value);
-      const { data, error } = await supabase
-        .rpc('set_claim', { uid, claim, value });
-      if (error) throw error;
-      return { data, error };
-    } catch (error) {
-      console.error('Error in set_claim:', error);
-    }
-  };
-
-  const DisplayUserAndRole = () => {
-    return (
-      <div>
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>{user.name}, Role: {user.roles.name}</li>
-          ))}
-        </ul>
-      </div>
-    )
-  }
-
-  return (
-    <div className="bg-[#1e1f1f] flex flex-col justify-center items-center">
-      <h1>Select testuser's role</h1>
-      <div>
-        <button
-          type="button"
-          onClick={() => selectRole("recruiter", userId)}
-          className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black"
-        >
-          Recruiter
-        </button>
-        {' '}
-        <button
-          type="button"
-          onClick={() => selectRole("student", userId)}
-          className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black"
-        >
-          Student
-        </button>
-      </div>
-      <DisplayUserAndRole />
-    </div>
-  )
-}
-
 const LoginSupabase = () => {
   const session = useSession()
   const navigate = useNavigate()
@@ -157,9 +70,7 @@ const LoginSupabase = () => {
       .select("is_new")
       .eq("id", session.user.id)
 
-    const isNew = data[0].is_new
-
-    if (isNew) {
+    if (data[0].is_new) {
       navigate('/firstlogin')
     }
   }
@@ -192,7 +103,6 @@ const LoginSupabase = () => {
     return (
       <div className="bg-[#1e1f1f] flex flex-col justify-center items-center h-screen">
         <h1>Logged in!</h1>
-        {/* <RoleSelectionButton session={session} /> */}
         <SignOutButton />
         <button
           type="button"
